238383f118a2f7642905b28cdf3771c43eda6895
SolaSJ <solasj@qq.com>
2018-03-28 11:46:30 +0800

    Swagger生成静态文档

--- src/main/java/com/sola/webdemo/controller/GoodController.java
+++ src/main/java/com/sola/webdemo/controller/GoodController.java
@@ -2,9 +2,14 @@ package com.sola.webdemo.controller;
 
 import com.sola.webdemo.entity.Good;
 import com.sola.webdemo.service.good.IGoodService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @description: GoodController
@@ -18,9 +23,10 @@ public class GoodController {
     @Autowired
     private IGoodService goodService;
 
-    @RequestMapping(value = "/{goodId}",method = RequestMethod.GET)
+    @RequestMapping(value = "/{goodId}", method = RequestMethod.GET)
     @ResponseBody
-    public Good getGood(@PathVariable(name = "goodId") Integer goodId) {
+    @ApiOperation(value = "获取商品信息")
+    public Good getGood(@ApiParam(value = "商品Id") @PathVariable(name = "goodId") Integer goodId) {
         return goodService.getGood(goodId);
     }
 
--- src/main/java/com/sola/webdemo/controller/JsonController.java
+++ src/main/java/com/sola/webdemo/controller/JsonController.java
@@ -1,21 +1,30 @@
 package com.sola.webdemo.controller;
 
+import com.sola.webdemo.config.CustomConfig;
 import com.sola.webdemo.util.JsonUtil;
-import org.springframework.stereotype.Controller;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @description: Json
  * @author: SolaSJ@qq.com
  * @create: 2018/03/21 17:45
  **/
-@Controller
+@RestController
+@Api(value = "/", tags = "Sola", description = "json操作接口")
 public class JsonController {
 
+    @Autowired
+    private CustomConfig customConfig;
+
     @PostMapping(value = "/json")
-    @ResponseBody
+    @ApiResponses(value = {@ApiResponse(code = 405, message = "Invalid input")})
     public String getStringNoBlank(String json) {
+        System.out.println(customConfig);
         return new JsonUtil().getStringNoBlank(json);
     }
 }
